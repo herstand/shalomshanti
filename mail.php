2296616360f4013c3843e065766c6b9b95253131
Micah Herstand <micah@herstand.com>
2015-12-14 00:34:08 -0500

    Improve email, add test data

--- mail.php
+++ mail.php
@@ -1,17 +1,30 @@
 <?php
 
-$row['Email addresses'] = "vidya.k.santosh@gmail.com";
-
 // require_once "db_access.php";
-// require_once getenv("SS_PEAR_PATH")."/Mail.php";
+require_once getenv("SS_PEAR_PATH")."/Mail.php";
 // $query = "SELECT `hashedId`, `Email addresses` FROM `".getenv('SS_DB_GUEST_TABLE')."`";
 // $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 // while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+$row['Email addresses'] = "micah@herstand.com";
+$row['hashedId'] = getenv("SS_TEST_ID");
+
     $from = "<wedding@shalomshanti.com>";
     $to = $row['Email addresses'];
     $bcc = ", wedding+bccsavethedate@shalomshanti.com";
-    $subject = "Save the date: July 10, 2016—Vidya and Micah's Wedding";
-    $body = "<a href=''><img width='640' src='https://www.shalomshanti.com/shalomshanti/savethedate.png' /></a><br><br>Save the date: July 10th, 2016 – Wedding of Vidya Santosh and Micah Herstand<br><br>If the above image is not loading, please click:<br>http://www.shalomshanti.com/shalomshanti/savethedate?response=-1&id={$row['hashedId']}";
+    $subject = "Save the date: July 10, 2016 – Wedding of Vidya Santosh and Micah Herstand";
+    $body = "
+      <a href='http://www.shalomshanti.com/shalomshanti/savethedate?response=-1&id={$row['hashedId']}'>
+          <img usemap='#responses' style='height:1000px; width:800px;' src='http://www.shalomshanti.com/shalomshanti/savethedate.png' />
+      </a>
+      <map name='responses'>
+        <area shape='circle' coords='193,870,55' href='http://www.shalomshanti.com/shalomshanti/savethedate?response=0&id={$row['hashedId']}' />
+        <area shape='circle' coords='331,870,55' href='http://www.shalomshanti.com/shalomshanti/savethedate?response=1&id={$row['hashedId']}' />
+        <area shape='circle' coords='470,870,55' href='http://www.shalomshanti.com/shalomshanti/savethedate?response=2&id={$row['hashedId']}' />
+        <area shape='circle' coords='608,870,55' href='http://www.shalomshanti.com/shalomshanti/savethedate?response=3&id={$row['hashedId']}' />
+      </map>
+      <br>If the above image is not loading, please use this link to access the website: <br>http://www.shalomshanti.com/shalomshanti/savethedate?response=-1&id={$row['hashedId']}
+";
+    
     $host = "ssl://smtp.gmail.com";
     $port = "465";
     $username = getenv("SS_EMAIL_USERNAME");
