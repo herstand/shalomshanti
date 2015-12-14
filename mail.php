6df3fcbe9e1ffba561dfe9a4f0b05df516c8f3d1
Micah Herstand <micah@herstand.com>
2015-12-13 22:49:26 -0500

    Bcc savethedate

--- mail.php
+++ mail.php
@@ -8,7 +8,7 @@ while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
   if ($row['hashedId'] === "9e1e94a20f7b652317418f7e3481b59db5f415c2a564b0a9ad6acade1a5d6afc") {
     $from = "<wedding@shalomshanti.com>";
     $to = $row['Email addresses'];
-    $bcc = ", wedding+vidyamicah@shalomshanti.com";
+    $bcc = ", wedding+bccsavethedate@shalomshanti.com";
     $subject = "Save the date: July 10, 2016—Vidya and Micah's Wedding";
     $body = "<img src='https://www.shalomshanti.com/shalomshanti/savethedate.png' /><br><br>Save the date: July 10th, 2016 – Wedding of Vidya Santosh and Micah Herstand<br><br>If the above image is not loading, please click:<br>http://www.shalomshanti.com/shalomshanti/savethedate?response=-1&id={$row['hashedId']}";
     $host = "ssl://smtp.gmail.com";
