8560caccb7a4e89452db10afe088f00b9d72dfaa
Micah Herstand <micah@herstand.com>
2015-12-23 12:07:54 -0500

    Add homepage

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="us-en">
+<head>
+<meta charset="utf-8">
+<meta title="Author" content="Vidya Santosh and Micah Herstand">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Shalom Shanti</title>
+<style type='text/css'>
+svg {
+  width:300px;
+  height:300px;
+  max-width:95%;
+  max-height:95%;
+  margin:10px auto;
+  display:block;
+}
+</style>
+</head>
+<body>
+<svg width="88px" height="100px" viewBox="0 0 88 100" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+    <defs>
+        <path id="path-1" d="M0,0.339784946 L99.583871,0.339784946 L99.583871,100 L0,100 L0,0.339784946 Z"></path>
+    </defs>
+    <g id="Home" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+        <g id="Form" sketch:type="MSArtboardGroup" transform="translate(-596.000000, -60.000000)">
+            <g id="Star" sketch:type="MSLayerGroup" transform="translate(590.000000, 60.000000)">
+                <path class='triangle-a' d="M35.6056452,75.1387097 L49.9599462,100 L64.313172,75.1387097 L35.6056452,75.1387097 Z" id="Fill-4" fill="#F4623A" sketch:type="MSShapeGroup"></path>
+                <path class='triangle-a' d="M6.77424731,25.225914 L21.1898387,50.1947312 L35.6059677,25.225914 L6.77424731,25.225914 Z" id="Fill-5" fill="#F4623A" sketch:type="MSShapeGroup"></path>
+                <path class='triangle-a' d="M93.145,25.2260215 L64.3132796,25.2260215 L78.728871,50.1948387 L93.145,25.2260215 Z" id="Fill-6" fill="#F4623A" sketch:type="MSShapeGroup"></path>
+                <path class='triangle-b' d="M49.9596774,0.339516129 L35.605914,25.2008065 L64.3134409,25.2008065 L49.9596774,0.339516129 Z" id="Fill-7" fill="#5EBAF5" sketch:type="MSShapeGroup"></path>
+                <path class='triangle-b' d="M78.729086,50.1698387 L64.3134946,75.1386559 L93.1446774,75.1386559 L78.729086,50.1698387 Z" id="Fill-8" fill="#5EBAF5" sketch:type="MSShapeGroup"></path>
+                <path class='triangle-b' d="M6.77413978,75.1386022 L35.6058602,75.1386022 L21.1902688,50.1697849 L6.77413978,75.1386022 Z" id="Fill-9" fill="#5EBAF5" sketch:type="MSShapeGroup"></path>
+                <path d="M64.3132796,25.2008065 L35.6057527,25.2008065 L21.1901613,50.1696237 L35.6057527,75.1384409 L64.3132796,75.1389785 L78.728871,50.1696237 L64.3132796,25.2008065 Z" id="Fill-10" fill="#FFFFFF" sketch:type="MSShapeGroup"></path>
+                <g id="Ganesha">
+                    <path d="M51.2117742,50.207957 C51.2117742,50.991828 50.5762903,51.6278495 49.7918817,51.6278495 C49.0080108,51.6278495 48.3719892,50.991828 48.3719892,50.207957 C48.3719892,49.424086 49.0080108,48.7880645 49.7918817,48.7880645 C50.5762903,48.7880645 51.2117742,49.424086 51.2117742,50.207957" id="Fill-11" fill="#3C4986" sketch:type="MSShapeGroup"></path>
+                    <path d="M42.948172,56.4401075 L47.9782796,60.2621505 L47.9782796,64.2922581 C47.9782796,64.2922581 47.776129,68.4884946 52.1917204,68.4884946 C56.6298925,68.4884946 56.6298925,64.2476344 56.6298925,64.2476344 L56.6298925,62.26 L54.0487097,62.26 L54.0325806,63.8427957 C54.0325806,63.8427957 54.0933333,65.5019355 52.8272043,65.5019355 C51.4949462,65.5019355 51.5997849,63.8669892 51.5997849,63.8669892 L51.5997849,60.2621505 L56.6298925,56.4401075 L56.6298925,44.0401075 L42.948172,44.0401075 L42.948172,56.4401075 Z" id="Fill-13" fill="#3D3A68" sketch:type="MSShapeGroup"></path>
+                    <path d="M49.7891935,33.9606452 L44.5816667,40.7998925 L54.9967204,40.7998925 L49.7891935,33.9606452 Z" id="Fill-15" fill="#3D3A68" sketch:type="MSShapeGroup"></path>
+                    <path d="M34.6095161,56.3662903 L41.7541398,52.2818817 L41.7541398,44.0399462 L34.6095161,39.9560753 L34.6095161,56.3662903 Z" id="Fill-17" fill="#3D3A68" sketch:type="MSShapeGroup"></path>
+                    <path d="M57.8632258,44.04 L57.8632258,52.2819355 L65.0083871,56.3663441 L65.0083871,39.9555914 L57.8632258,44.04 Z" id="Fill-19" fill="#3D3A68" sketch:type="MSShapeGroup"></path>
+                    <mask id="mask-2" sketch:name="Clip 22" fill="white">
+                        <use xlink:href="#path-1"></use>
+                    </mask>
+                    <g id="Clip-22"></g>
+                    <path d="M42.9483871,42.8483871 L56.6301075,42.8483871 L56.6301075,41.6854839 L42.9483871,41.6854839 L42.9483871,42.8483871 Z" id="Fill-21" fill="#3D3A68" sketch:type="MSShapeGroup" mask="url(#mask-2)"></path>
+                    <path d="M47.2969892,50.1696774 C47.2969892,50.5680645 46.9744086,50.8906452 46.5760215,50.8906452 C46.178172,50.8906452 45.8550538,50.5680645 45.8550538,50.1696774 C45.8550538,49.771828 46.178172,49.4487097 46.5760215,49.4487097 C46.9744086,49.4487097 47.2969892,49.771828 47.2969892,50.1696774" id="Fill-23" fill="#FFFFFF" sketch:type="MSShapeGroup" mask="url(#mask-2)"></path>
+                    <path d="M53.6335484,50.1696774 C53.6335484,50.5680645 53.3109677,50.8906452 52.9125806,50.8906452 C52.5147312,50.8906452 52.1916129,50.5680645 52.1916129,50.1696774 C52.1916129,49.771828 52.5147312,49.4487097 52.9125806,49.4487097 C53.3109677,49.4487097 53.6335484,49.771828 53.6335484,50.1696774" id="Fill-24" fill="#FFFFFF" sketch:type="MSShapeGroup" mask="url(#mask-2)"></path>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
+</body>
+</html>
