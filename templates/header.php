840faddbebd153f45fa5bc2b733f3ee6b2bd2c85
Micah Herstand <Micah@Herstand.com>
2016-04-06 17:38:43 -0400

    Remove psw"

--- templates/header.php
+++ templates/header.php
@@ -21,7 +21,7 @@ if (isset($session->user)) {
           ?><li class="plan"><a class="typ-littleTitle" data-long-name="Plan your trip" data-short-name="Planning" href="plan-your-trip">Planning</a></li><?php
           ?><li class="rsvp<?php echo $complete; ?>"><a class="typ-littleTitle" href="rsvp">RSVP</a></li><?php
         } else {
-          ?><li><form class="login" action="/API/Login" method="POST"><input class="typ-littleTitle" placeholder="Enter Guest Code" type="text" value="test" /><button type="submit"><svg id="nav" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30.18 30"><defs><style>.cls-2{fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:2px;}</style></defs><title>login-submit</title><rect class="fill" width="30.18" height="30"/><line class="cls-2" x1="21.66" y1="14.76" x2="7" y2="14.76"/><polyline class="cls-2" points="16.43 9.52 21.66 14.76 16.43 20"/></svg></button></li></form><?php
+          ?><li><form class="login" action="/API/Login" method="POST"><input class="typ-littleTitle" placeholder="Enter Guest Code" type="text" value="" /><button type="submit"><svg id="nav" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30.18 30"><defs><style>.cls-2{fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:2px;}</style></defs><title>login-submit</title><rect class="fill" width="30.18" height="30"/><line class="cls-2" x1="21.66" y1="14.76" x2="7" y2="14.76"/><polyline class="cls-2" points="16.43 9.52 21.66 14.76 16.43 20"/></svg></button></li></form><?php
         }
 
     ?></ul><?php
