df06025cd0f8375abaec4a20b96fb726855b40be
Micah Herstand <Micah@Herstand.com>
2016-04-07 11:20:06 -0400

    Update close x

--- css/modal.css.php
+++ css/modal.css.php
@@ -4,7 +4,7 @@ require_once "css/variables.php";
 ?>
 .modalWrapper { z-index:11; background:rgba(0,0,0,.3); position:fixed; left:0; top:0; width:100%; height:100%; }
 .modalWrapper .modal { text-align:center; width:calc(100vw - 13.33vw); max-width:415px; background:white; margin:0 auto; position:relative; left:0; top:50%; margin-top:-200px; padding:40px 6.67vw; }
-.modalWrapper .modal .close { position:absolute; right:40px; top:20px; font-size:32px; line-height:16px; }
+.modalWrapper .modal .close { position:absolute; right:20px; top:0px; font-size:42px; line-height:42px; color:<?php echo $blue; ?>; }
 .modalWrapper .modal p:last-of-type { margin-bottom:30px; }
 .modalWrapper .modal h4 { margin-bottom:30px; }
 .modalWrapper .modal .householdName { margin-bottom:20px; }
