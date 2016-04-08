4d15ef7f164f529f44ca709cc66311a78f4b50b5
Micah Herstand <Micah@Herstand.com>
2016-04-08 03:48:17 -0400

    Update styles

--- css/nav-primary.css.php
+++ css/nav-primary.css.php
@@ -2,7 +2,7 @@ body > header > nav.primary { margin-top:0px; margin-bottom:0px; }
 body > header > nav.primary > ul { width:100%; text-align:right; }
 body > header > nav.primary > ul > li { vertical-align:top; padding-top:34px; }
 body > header > nav.primary > ul > li:not(:last-of-type):not(.homepage) { margin-right:1px; }
-body > header > nav.primary > ul > li.homepage, body > header > nav.primary.loggedIn > ul > li.homepage { position:absolute; left:6.67vw; top:0px; padding-top:20px; }
+body > header > nav.primary > ul > li.homepage, body > header > nav.primary.loggedIn > ul > li.homepage { position:absolute; top:0px; padding-top:20px; }
 body > header > nav.primary > ul > li a { padding-bottom:6px; }
 body > header > nav.primary.loggedIn > ul > li.rsvp { padding-top:0px; }
 body > header > nav.primary > ul > li.rsvp a { display:inline-block; padding:34px 13px 20px 13px; background-color:<?php echo $orange; ?>; }
@@ -30,4 +30,20 @@ body > header > nav.primary > ul > li.logo a img { height:50px; }
   body > header > nav.primary.loggedIn > ul > li { padding-top:43px; }
   body > header > nav.primary > ul > li.homepage { left:40px; }
   body > header > nav.primary > ul > li.logo a img { height:70px; }
+}
+
+body > header > nav.primary > ul > li.homepage, body > header > nav.primary.loggedIn > ul > li.homepage {
+  left:<?php echo $padding_mobile; ?>;
+}
+
+@media (min-width:<?php echo $mobile3; ?>px) {
+  body > header > nav.primary > ul > li.homepage, body > header > nav.primary.loggedIn > ul > li.homepage {
+    left:<?php echo $padding_mobile3; ?>;
+  }
+}
+
+@media (min-width:<?php echo $ipadLandscape; ?>px) {
+  body > header > nav.primary > ul > li.homepage, body > header > nav.primary.loggedIn > ul > li.homepage {
+    left:<?php echo $padding_ipadLandscape; ?>;
+  }
 }
