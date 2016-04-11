58ad43a214d111488ea6a6e21697be839a852a91
Micah Herstand <Micah@Herstand.com>
2016-04-11 14:09:01 -0400

    Update css

--- css/footer.css.php
+++ css/footer.css.php
@@ -13,7 +13,7 @@ body > footer p.typ-body { margin-bottom:0px; }
 body > footer header { margin-bottom:15px; /* text below has 5px of extra line-height, so ends up being 20px */ }
 body > footer .content { width:100%; padding-left:6.67%; padding-right:6.67%; }
 body > footer .content span:not(:last-of-type) { vertical-align:middle; margin-right:5px; }
-body > footer .content .typ-littleTitle { font-size:15px; }
+body > footer .content .typ-littleTitle { font-size:13px; }
 
 /* Content & Animation */
 body > footer .content span:not(:last-of-type):after { content:"·"; }
