c27194f927364416811c72aff37a6f6d83ff95ff
Micah Herstand <Micah@Herstand.com>
2016-04-12 01:06:06 -0400

    CSS changes

--- css/modal.css.php
+++ css/modal.css.php
@@ -5,15 +5,14 @@ require_once "css/variables.php";
 .modalWrapper { z-index:11; background:rgba(0,0,0,.3); position:fixed; left:0; top:0; width:100%; height:100%; }
 .modalWrapper .modal { text-align:center; width:calc(100vw - 13.33vw); max-width:415px; background:white; margin:0 auto; position:relative; left:0; top:2%; padding:40px 6.67vw; }
 .modalWrapper .modal .close { position:absolute; right:20px; top:0px; font-size:42px; line-height:42px; color:<?php echo $blue; ?>; }
-.modalWrapper .modal p:last-of-type { margin-bottom:30px; }
 .modalWrapper .modal h4 { margin-bottom:30px; }
 .modalWrapper .modal .householdName { margin-bottom:20px; }
-.modalWrapper .modal .rsvp { display:inline-block; line-height:40px; width:170px; height:40px; border:2px solid <?php echo $orange; ?>; }
+.modalWrapper .modal .rsvp { margin:30px auto; display:inline-block; line-height:40px; width:170px; height:40px; border:2px solid <?php echo $orange; ?>; }
 
 .modalWrapper .modal .rsvp:hover { color:white; border: 2px solid <?php echo $orange; ?>; background-color:<?php echo $orange; ?>; }
 
 @media (min-width:<?php echo $iphone6; ?>px) {
-  .modalWrapper .modal { top:50%; margin-top:-200px; }
+  .modalWrapper .modal { top:50%; margin-top:-267px; }
 }
 @media (min-width:<?php echo $mobile3; ?>px) {
   .modalWrapper .modal { padding-left:40px; padding-right:40px; }
--- templates/loginModal.php
+++ templates/loginModal.php
@@ -2,9 +2,10 @@
   <div class="modal">
   <button class="close">×</button>
   <h4 class="typ-littleTitle">Welcome!</h4>
-  <p class="typ-body">Welcome to the guests-only area of our website! Did you recognize your password? It's a word from Indian or Jewish culture. See if you can figure out what it means!</p><p class="typ-body">You’re logged in as:</p>
+  <p class="typ-body">Welcome to the guests-only area of our website! You’re logged <span class="nobreak">in as:</span></p>
   <p class="householdName typ-subsection-header"><em class="household_name"></em></p>
   <p class="typ-body">We’ve put together lots of information for you to browse. Please be sure to RSVP by <span class="nobreak">May 5.</span></p>
   <a href="/rsvp" class="typ-littleTitle rsvp">RSVP Now</a>
+  <p class="typ-body">P.S. Did you recognize your password? It's a word from Indian or Jewish culture. A quick web search might reveal something you didn't <span class="nobreak">know before!</span></p>
   </div>
 </div>
