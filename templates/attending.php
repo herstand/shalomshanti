720d2cad548dee36f78c1b9ba0438b6d05aad614
Micah Herstand <Micah@Herstand.com>
2016-04-13 00:36:13 -0400

    Update look of totals

--- templates/attending.php
+++ templates/attending.php
@@ -3,50 +3,44 @@
 echo "<section class='havdalah'>";
 echo "<h2>Havdalah / Mehendi</h2>";
 echo "<h3>Vidya</h3>";
-echo "<h4>Attendants</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `havdalah_guest_attendants` WHERE `havdalah_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id and List = 'Vidya'";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Attendants: {$row['receptionAdultExpected']}</h4>";
 
-echo "<h4>Households</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `havdalah_guest_attendants` WHERE `havdalah_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id and List = 'Vidya'";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Households: {$row['receptionAdultExpected']}</h4>";
 
 echo "<h3>Micah</h3>";
-echo "<h4>Attendants</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `havdalah_guest_attendants` WHERE `havdalah_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id and List = 'Micah'";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Attendants: {$row['receptionAdultExpected']}</h4>";
 
-echo "<h4>Households</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `havdalah_guest_attendants` WHERE `havdalah_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id and List = 'Micah'";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Households: {$row['receptionAdultExpected']}</h4>";
 
 echo "<hr>";
 echo "<h3>Total</h3>";
-echo "<h4>Attendants</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `havdalah_guest_attendants` WHERE `havdalah_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Attendants: {$row['receptionAdultExpected']}</h4>";
 
-echo "<h4>Households</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `havdalah_guest_attendants` WHERE `havdalah_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Households: {$row['receptionAdultExpected']}</h4>";
 
 
 echo "</section><section class='ceremony'>";
@@ -55,50 +49,44 @@ echo "</section><section class='ceremony'>";
 
 echo "<h2>Ceremony</h2>";
 echo "<h3>Vidya</h3>";
-echo "<h4>Attendants</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `ceremony_guest_attendants` WHERE `ceremony_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id and List = 'Vidya'";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Attendants: {$row['receptionAdultExpected']}</h4>";
 
-echo "<h4>Households</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `ceremony_guest_attendants` WHERE `ceremony_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id and List = 'Vidya'";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Households: {$row['receptionAdultExpected']}</h4>";
 
 echo "<h3>Micah</h3>";
-echo "<h4>Attendants</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `ceremony_guest_attendants` WHERE `ceremony_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id and List = 'Micah'";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Attendants: {$row['receptionAdultExpected']}</h4>";
 
-echo "<h4>Households</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `ceremony_guest_attendants` WHERE `ceremony_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id and List = 'Micah'";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Households: {$row['receptionAdultExpected']}</h4>";
 
 echo "<hr>";
 echo "<h3>Total</h3>";
-echo "<h4>Attendants</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `ceremony_guest_attendants` WHERE `ceremony_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Attendants: {$row['receptionAdultExpected']}</h4>";
 
-echo "<h4>Households</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `ceremony_guest_attendants` WHERE `ceremony_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Households: {$row['receptionAdultExpected']}</h4>";
 
 
 
@@ -112,50 +100,44 @@ echo "</section><section class='reception'>";
 echo "<h2>Reception</h2>";
 
 echo "<h3>Vidya</h3>";
-echo "<h4>Attendants</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `reception_guest_attendants` WHERE `reception_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id and List = 'Vidya'";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Attendants: {$row['receptionAdultExpected']}</h4>";
 
-echo "<h4>Households</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `reception_guest_attendants` WHERE `reception_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id and List = 'Vidya'";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Households: {$row['receptionAdultExpected']}</h4>";
 
 echo "<h3>Micah</h3>";
-echo "<h4>Attendants</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `reception_guest_attendants` WHERE `reception_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id and List = 'Micah'";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Attendants: {$row['receptionAdultExpected']}</h4>";
 
-echo "<h4>Households</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `reception_guest_attendants` WHERE `reception_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id and List = 'Micah'";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Households: {$row['receptionAdultExpected']}</h4>";
 
 echo "<hr>";
 echo "<h3>Total</h3>";
-echo "<h4>Attendants</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `reception_guest_attendants` WHERE `reception_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Attendants: {$row['receptionAdultExpected']}</h4>";
 
-echo "<h4>Households</h4>";
 $query = "SELECT COUNT(*) as receptionAdultExpected
 FROM `".getenv('SS_DB_GUEST_TABLE')."`, `reception_guest_attendants` WHERE `reception_guest_attendants`.guest_id = ".getenv('SS_DB_GUEST_TABLE').".id";
 $result = $mysqli->query($query) or trigger_error($mysqli->error."[$query]");
 $row = $result->fetch_array(MYSQLI_ASSOC);
-echo "<strong>{$row['receptionAdultExpected']}</strong><br>";
+echo "<h4>Households: {$row['receptionAdultExpected']}</h4>";
 echo "</section>";
 
 ?>
