b154eb61798051eefca38610db5881a4f3edf81f
Micah Herstand <Micah@Herstand.com>
2016-04-15 16:56:47 -0400

    Remove default webkit styles on text inputs

--- css/custom-reset.css.php
+++ css/custom-reset.css.php
@@ -18,6 +18,7 @@ main { position:relative; text-align:center; }
 figure { margin:0; padding:0; }
 fieldset { border:0px; margin:0; padding:0; }
 input { border:0px; border-radius:0px; box-shadow:none; }
+input[type="text"] { -webkit-appearance: none; }
 input:focus, button:focus, label:focus { outline:none; }
 button { background:none; border:none; padding:0; }
 button:hover { cursor:pointer; }
