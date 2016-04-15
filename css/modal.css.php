aea96ec22898d2ef31516e9d09984e4ef443ec69
Micah Herstand <Micah@Herstand.com>
2016-04-15 18:49:40 -0400

    update close button font weight

--- css/modal.css.php
+++ css/modal.css.php
@@ -4,7 +4,7 @@ require_once "css/variables.php";
 ?>
 .modalWrapper { z-index:11; background:rgba(0,0,0,.3); position:fixed; left:0; top:0; width:100%; height:100%; overflow:auto; }
 .modalWrapper .modal { text-align:center; width:calc(100vw - 13.33vw); max-width:415px; background:white; margin:0 auto; position:relative; left:0; top:2%; padding:40px 6.67vw; margin-bottom:20px; }
-.modalWrapper .modal .close { position:absolute; right:20px; top:0px; font-size:42px; line-height:42px; color:<?php echo $blue; ?>; }
+.modalWrapper .modal .close { position:absolute; right:20px; top:0px; font-size:42px; line-height:42px; color:<?php echo $blue; ?>; font-weight:100; }
 .modalWrapper .modal h4 { margin-bottom:30px; }
 .modalWrapper .modal .householdName { margin-bottom:20px; }
 .modalWrapper .modal .rsvp { margin:30px auto; display:inline-block; line-height:40px; width:170px; height:40px; border:2px solid <?php echo $orange; ?>; }
